abc48df238f074e3bd2bfc9ad7b5be3f1eacce98
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-05-05 16:08:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-technotab-server
---
+++ src/test/java/com/jakobniinja/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakobniinja.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
